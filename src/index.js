2e150991c37686564eaaf0c1b22e8c524a4fa377
Casey Harding <casey.r.harding@gmail.com>
2022-03-10 17:55:59 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("ALAS");
+import axios from "axios";
+
+axios.get("https://randomuser.me/api/?results=5")
+  .then(res => {
+    console.log(res.data.results);
+  }).catch(err => console.error);
