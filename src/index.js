524213f4a0de77f729aa53210ac6fb696f9410d7
Casey Harding <casey.r.harding@gmail.com>
2022-03-10 18:04:37 -0500

    Elements created.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,29 @@ axios.get("https://randomuser.me/api/?results=5")
   .then(res => {
     console.log(res.data.results);
   }).catch(err => console.error);
+
+axios.get("blah.com").then(console.log("what?"))
+
+  /**
+   *  <h3>Username</h3>
+      <p>Email</p>
+      <p>Age + Gender</p>
+      <img 
+        src="https://downloadwallpaper.org/wp-content/uploads/2016/05/thor-wallpaper3.jpg" 
+        alt="THOR" 
+      />
+   */
+
+function makeUser(user) {
+
+}
+
+makeUser(
+  { 
+    username: "CRHarding", 
+    email: "case@case.com", 
+    age: 74, 
+    gender: "god", 
+    imgSrc: "https://downloadwallpaper.org/wp-content/uploads/2016/05/thor-wallpaper3.jpg" 
+  }
+)
